d567c8cd23aadf88342eb9936f6f7b79ff9327e0
boymeetscode777 <fuzomntungwa3@gmail.com>
2024-06-18 21:55:48 +0200

    ...

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(array,argToRemove) {
+    const newArray=[];
+
+    for (let i = 0; i < array.length; i++){
+        if (array[i]!==n){
+            newArray.push(array[i])
+        }
+    }
+    return newArray;
 };
 
 // Do not edit below this line
