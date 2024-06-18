17d466d35a1f9487163acf1f31a08ed9d0f008a5
boymeetscode777 <fuzomntungwa3@gmail.com>
2024-06-18 19:59:04 +0200

    ....

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,10 +1,10 @@
-repeatString(string,num); {
-    if (num < 0){return "Error!"};
-    let word = "";
-    for (let i = 0; i < num; i++) {
-       word += string;
+const repeatString = function (word, times) {
+    if (times < 0) return "ERROR";
+    let string = "";
+    for (let i = 0; i < times; i++) {
+      string += word;
     }
-        return word;
-}
-
-module.exports = repeatString;
+    return string;
+  };
+  
+  module.exports = repeatString;
