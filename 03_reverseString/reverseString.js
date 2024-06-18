4808dd4eba7d3c0f63ae843662833d69deb72d63
boymeetscode777 <fuzomntungwa3@gmail.com>
2024-06-18 21:11:42 +0200

    .

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(stringToSplit) {
+    const splitToArray = stringToSplit.split();
+    const reverseArray = splitToArray.reverse();
+    const joinToString = reverseArray.join();
+    return joinToString
 };
 
 // Do not edit below this line
