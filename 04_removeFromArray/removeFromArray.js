b4ab78504c2f3e35a7226cc3968caa3a9079c467
boymeetscode777 <fuzomntungwa3@gmail.com>
2024-06-19 19:01:46 +0200

    ...

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,13 +1,12 @@
-const removeFromArray = function(array,argToRemove) {
+const removeFromArray = function(array, arg1, arg2, arg3, arg4) {
     const newArray=[];
 
     for (let i = 0; i < array.length; i++){
-        if (array[i]!==argToRemove){
+        if (array[i]!==arg1 && array[i]!==arg2 && array[i]!==arg3 && array[i]!==arg4){
             newArray.push(array[i])
-        }
-    }
     return newArray;
-};
+  };
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
