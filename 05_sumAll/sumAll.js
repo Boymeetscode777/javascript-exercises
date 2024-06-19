ea5dedd1f5e7287b3c70a20ebbd75eeff2772389
boymeetscode777 <fuzomntungwa3@gmail.com>
2024-06-19 20:49:44 +0200

    ...

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,6 @@
 const sumAll = function(firstNum, lastNum) {
+    if (!Number.isInteger(firstNum) || !Number.isInteger(lastNum)) return "ERROR";
+    if ((firstNum < 0) || (lastNum <0)) return "ERROR"; 
     let finalSum = 0;
     for (let i = firstNum; i <= lastNum; i++){
         finalSum += i;
