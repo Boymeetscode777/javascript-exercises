f6039f6adb0dc740d71fef9d11b93580bc4e22c0
boymeetscode777 <fuzomntungwa3@gmail.com>
2024-06-19 20:43:24 +0200

    ...

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,9 @@
-const sumAll = function() {
-
+const sumAll = function(firstNum, lastNum) {
+    let finalSum = 0;
+    for (let i = firstNum; i <= lastNum; i++){
+        finalSum += i;
+    }
+    return finalSum
 };
 
 // Do not edit below this line
