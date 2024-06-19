81bbd55e72708f9d3bdbb7185f650e11681454b0
boymeetscode777 <fuzomntungwa3@gmail.com>
2024-06-19 21:40:38 +0200

    ...

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,12 +1,10 @@
 const leapYears = function(year) {
-    if (year%4===0){
+    if (year%4===0 && year!==400){
         return year + " is a leap year"
     } else {
         return year + " is not a leap year"
     }
 };
-
-console.log(leapYears(2000))
 
 // Do not edit below this line
 module.exports = leapYears;
