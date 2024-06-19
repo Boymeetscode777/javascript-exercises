d33059860a680d1656f855543ed3fab9c335fd3c
boymeetscode777 <fuzomntungwa3@gmail.com>
2024-06-19 22:28:10 +0200

    .

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempInFahrenheit) {
+  toCelcius=(tempInFahrenheit-32)*(5/9);
+  return Number(toCelcius).toFixed(1);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempInCelcius) {
+  toFahrenheit=(tempInCelcius*(9/5)+32);
+  return Number(toFahrenheit).toFixed(1)
 };
 
 // Do not edit below this line
