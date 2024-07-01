f8db021d5df264b6161c8c1efd10ad58df054967
boymeetscode777 <fuzomntungwa3@gmail.com>
2024-07-01 21:45:12 +0200

    ...

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,32 @@
-const add = function() {
-	
+const add = function (a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	return array.reduce((total, current) => total + current, 0)
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((total, current) => total * current)
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
-};
+const factorial = function(number) {
+	if (number === 0){return 1}
+  let product = 1
+  for (let i = number; i > 0; i--){
+    product*=i
+  }
+  return product
+  }
+
 
 // Do not edit below this line
 module.exports = {
