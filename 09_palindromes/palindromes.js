399d66abea2c7a43612da90f058de6a1b199bf21
boymeetscode777 <fuzomntungwa3@gmail.com>
2024-07-01 23:12:45 +0200

    ...

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    const lowerCaseString = string.toLowerCase();
+    const noPunctuationString = lowerCaseString.replace(/[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~\s]/g, '');
+    const splitArr = noPunctuationString.split("");
+    const reverseArr = splitArr.reverse();
+    const joinArr = reverseArr.join("")
+    
+    
+    return joinArr === noPunctuationString
 };
 
 // Do not edit below this line
