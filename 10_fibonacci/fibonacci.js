19a5e7e24e30f2f1eaa7fca0d05a36c937686808
boymeetscode777 <fuzomntungwa3@gmail.com>
2024-07-02 00:40:20 +0200

    ....

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function(number) {
+    if (number < 0) return "OOPS"
+    number = parseInt(number)
 
+    if (number === 0) return 0;
+    if (number === 1) return 1;
+
+    let a = 0
+    let b = 1
+
+    for (let i = 2; i <= number; i++){
+      temp = a + b;
+      a = b;
+      b = temp
+    }
+        return b
 };
 
 // Do not edit below this line
